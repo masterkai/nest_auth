b53ffeaa945bae800a6dc525861e3d0cc62b56e0
masterkai <kai0919@hotmail.com>
2022-06-28 23:44:24 +0800

    add: Entities

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'nest_auth',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+})
+export class AppModule {}
